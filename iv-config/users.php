4047c68100ad8208ebf589790753cea6a2aa278f
zhourong007 <zhourong007@gmail.com>
2018-02-20 13:38:28 +0800

    Signed-off-by: zhourong007 <zhourong007@gmail.com>

---
+++ iv-config/users.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Userlist file, passwords is sha1 hash. To manually change passwords
+ * and generate hashes you can use http://sha1-hash-online.waraxe.us/
+ * or use any other sha1 online hash calculator
+ */
+
+ 	$users['admin']['password'] = '6977dc0676f8153b2f33ce7fcac5cd6fe8a84d66';
+	$users['admin']['access'] = '*';
+?>
